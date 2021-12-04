7aa55a3414fa2c454af1edeb7bef307f526f7483
mogannam <JoeMogannam@gmail.com>
2021-12-04 14:08:55 -0500

    add sql files

--- db/schema.sql
+++ db/schema.sql
@@ -3,38 +3,52 @@ DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
 
-CREATE TABLE parties (
+drop table if exists employee;
+drop table if exists role;
+drop table if exists department;
+
+
+CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
+  name VARCHAR(30) NOT NULL UNIQUE
 );
 
-CREATE TABLE candidates (
+
+create table role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  party_id INTEGER,
-  industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party
-    FOREIGN KEY (party_id)
-    REFERENCES parties(id)
+  title VARCHAR(30) NOT NULL UNIQUE,
+  salary decimal(10) NOT NULL,
+  departmentId INTEGER,
+  CONSTRAINT fk_department
+    FOREIGN KEY (departmentId)
+    REFERENCES department(id)
     ON DELETE SET NULL
 );
 
-CREATE TABLE voters (
+CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  firstName varchar(30) NOT NULL,
+  lastName varchar(30) NOT NULL,
+  roleId integer,
+  managerId integer,
+  CONSTRAINT fk_roleId FOREIGN KEY (roleId) REFERENCES role(id) ON DELETE CASCADE
+  -- CONSTRAINT fk_managerId FOREIGN KEY (managerId) REFERENCES employee(id) ON DELETE CASCADE
 );
 
-CREATE TABLE votes (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  voter_id INTEGER NOT NULL,
-  candidate_id INTEGER NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT uc_voter UNIQUE (voter_id),
-  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
-  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
-);
+-- CREATE TABLE voters (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   first_name VARCHAR(30) NOT NULL,
+--   last_name VARCHAR(30) NOT NULL,
+--   email VARCHAR(50) NOT NULL,
+--   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- CREATE TABLE votes (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   voter_id INTEGER NOT NULL,
+--   candidate_id INTEGER NOT NULL,
+--   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+--   CONSTRAINT uc_voter UNIQUE (voter_id),
+--   CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+--   CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+-- );
